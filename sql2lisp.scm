f919c88da0e257a6c940379dcee6086996e10067
Yoichi Hirai <yh@pamuk.(none)>
2010-04-06 15:20:08 +0900

    insert, update and delete implemented

--- sql2lisp.scm
+++ sql2lisp.scm
@@ -3,7 +3,7 @@
 ;しかし，なんか逆のことをしている気がしてきたので撤退．
 
 (use gauche.collection)
-
+(use srfi-1)
 
 ;;; an example lisp database
 ; TODO: separate interaface from implementation
@@ -36,8 +36,40 @@
 ; (select table0 (where= 'age 3))
 ; => (((name . "kame") (age . 3)))
 (define (where= symb value)
-  (where-pred (list symb) (lambda (x) (= x value))))
+  (where-pred (list symb) (lambda (x) (equal? x value))))
 
+
+;delete implementation
+; (delete table0 (where= 'age 3))
+; => (((name . "kuro") (age . 5)))
+(define (delete table where)
+  (filter (lambda (x) (not (where x))) table))
+
+;insert implementation
+; (insert '((name . "jiro")) table0)
+; (((name . "jiro")) ((name . "kame") (age . 3)) ((name . "kuro") (age . 5)))
+(define insert cons)
+
+;update implementation
+; (update table0 '((name . "taro")) (where= 'name "kuro"))
+; => (((name . "kame") (age . 3))
+;     ((name . "taro") (age . 5)))
+
+(define (update table tuple where)
+  (map
+   (lambda (t)
+	 (if (where t)
+		 (update-tuple tuple t)
+		 t))
+   table))
+
+(define (update-tuple new old)
+  (if (null? new)
+	  old
+	  (let* ((head (car new))
+			 (key  (car head))
+			 (val  (cdr head)))
+		(acons key val (alist-delete key old)))))
 
 ;;; SQL parser
 
