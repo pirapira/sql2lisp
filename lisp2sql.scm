a73aeea3c7009352bbd239963c6d4427a610e052
Yoichi Hirai <yh@pamuk.(none)>
2010-04-21 22:25:07 +0900

    comparing lisp and SQL

--- lisp2sql.scm
+++ lisp2sql.scm
@@ -9,35 +9,63 @@
 (use dbd.sqlite3)
 (use gauche.collection)
 
-(define (exec-sql sql)
-  (let* ((conn (dbi-connect "dbi:sqlite3:db=:memory:"))
+(define (create-db)
+  (let ((conn (dbi-connect "dbi:sqlite3:db=:memory:"))
 		 )
 	(dbi-do conn "CREATE TABLE hoge (num int)")
 	(dbi-do conn "INSERT INTO hoge VALUES (8)")
+	conn))
+
+(define (exec-sql sql)
+  (let ((conn (create-db)))
 	(dbi-do conn sql)))
+
+(define (exec-lisp lisp)
+	(eval 
+	 `(let ((conn (create-db))
+			(hoge
+			 (lambda ()
+			   (map values (dbi-do conn "SELECT * from hoge"))))
+			)
+		,lisp)
+	 (interaction-environment)))
 
 (define (map-equal? a b)
   (equal?
    (map values a)
    (map values b)))
 
+
+;; a test to succeed
 (define (test-exec-sql)
   (define (test-exec-sql-inner sql)
 	(map-equal? (exec-sql sql) (exec-sql sql)))
   (test-exec-sql-inner "select * from hoge"))
 
-
-;(define (test sql lisp)
-;  (equal? (exec-sql sql) (exec-lisp lisp)))
-
+;; a test to fail
 (define (failing-test) #f)
+
+;; the main tests
+(define (compare-sql-lisp sql lisp)
+  (map-equal? (exec-sql sql) (exec-lisp lisp)))
+
+(define (sql-lisp-simplest-test)
+  (compare-sql-lisp
+   "select * from hoge"
+   '(hoge)))
 
 (define tests
   (list
    test-exec-sql
+   sql-lisp-simplest-test
 ;   failing-test
    ))
 
-(for-each
- (lambda (x) (or (x) (error 'test-failed)))
- tests)
+(define (test)
+  (for-each
+   (lambda (x) (or (x) (error 'test-failed)))
+   tests))
+
+(test)
+
+  
