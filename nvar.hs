74392edcb59382b3c2ddccf4182886ad9936149a
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2010-07-26 13:53:46 +0900

    nvar.

--- nvar.hs
+++ nvar.hs
@@ -14,17 +14,14 @@
 --
 -----------------------------------------------------------------------------
 
-module Control.Concurrent.SampleVar
+module NVar
        (
-         -- * Sample Variables
-         SampleVar,         -- :: type _ =
+         -- * Nonblocking Variables
+         NVar,         -- :: type _ =
  
-         newEmptySampleVar, -- :: IO (SampleVar a)
-         newSampleVar,      -- :: a -> IO (SampleVar a)
-         emptySampleVar,    -- :: SampleVar a -> IO ()
-         readSampleVar,     -- :: SampleVar a -> IO a
-         writeSampleVar,    -- :: SampleVar a -> a -> IO ()
-         isEmptySampleVar,  -- :: SampleVar a -> IO Bool
+         newNVar,      -- :: (a -> a -> a) -> a -> IO (NVar a)
+         readNVar,     -- :: NVar a -> IO (a)
+         writeNVar,    -- :: NVar a -> a -> IO ()
 
        ) where
 
@@ -33,85 +30,41 @@ import Prelude
 import Control.Concurrent.MVar
 
 -- |
--- Sample variables are slightly different from a normal 'MVar':
+-- NVar behaves differently than a normal 'MVar':
 -- 
---  * Reading an empty 'SampleVar' causes the reader to block.
---    (same as 'takeMVar' on empty 'MVar')
+--  * Reading an NVar returns the current value. NVar is unchanged.
 -- 
---  * Reading a filled 'SampleVar' empties it and returns value.
---    (same as 'takeMVar')
--- 
---  * Writing to an empty 'SampleVar' fills it with a value, and
---    potentially, wakes up a blocked reader (same as for 'putMVar' on
---    empty 'MVar').
+--  * Writing to an empty NVar fills it with a value.
 --
---  * Writing to a filled 'SampleVar' overwrites the current value.
---    (different from 'putMVar' on full 'MVar'.)
+--  * Writing to a filled 'NVar' makes NVar filled with
+--    a value larger or equal to those already written and the newly written value.
+--
+--  * The type a must be of Ord.
 
-type SampleVar a
- = MVar (Int,           -- 1  == full
-                        -- 0  == empty
-                        -- <0 no of readers blocked
-          MVar a)
-
--- |Build a new, empty, 'SampleVar'
-newEmptySampleVar :: IO (SampleVar a)
-newEmptySampleVar = do
-   v <- newEmptyMVar
-   newMVar (0,v)
+type NVar a
+ = MVar (a)
 
 -- |Build a 'SampleVar' with an initial value.
-newSampleVar :: a -> IO (SampleVar a)
-newSampleVar a = do
-   v <- newMVar a
-   newMVar (1,v)
+newNVar :: a -> IO (NVar a)
+newNVar v = newMVar (v)
 
--- |If the SampleVar is full, leave it empty.  Otherwise, do nothing.
-emptySampleVar :: SampleVar a -> IO ()
-emptySampleVar v = do
-   (readers, var) <- takeMVar v
-   if readers > 0 then do
-     _ <- takeMVar var
-     putMVar v (0,var)
-    else
-     putMVar v (readers,var)
-
--- |Wait for a value to become available, then take it and return.
-readSampleVar :: SampleVar a -> IO a
-readSampleVar svar = do
+-- |Non-blocking read.
+readNVar :: NVar a -> IO a
+readNVar svar = do
 --
 -- filled => make empty and grab sample
 -- not filled => try to grab value, empty when read val.
 --
-   (readers,val) <- takeMVar svar
-   putMVar svar (readers-1,val)
-   takeMVar val
+   val <- takeMVar svar
+   putMVar svar val
+   return val
 
--- |Write a value into the 'SampleVar', overwriting any previous value that
--- was there.
-writeSampleVar :: SampleVar a -> a -> IO ()
-writeSampleVar svar v = do
+-- |Write a value into the 'NVar' if the new value is larger.
+writeNVar :: Ord a => NVar a -> a -> IO ()
+writeNVar svar v = do
 --
 -- filled => overwrite
 -- not filled => fill, write val
 --
-   (readers,val) <- takeMVar svar
-   case readers of
-     1 -> 
-       swapMVar val v >> 
-       putMVar svar (1,val)
-     _ -> 
-       putMVar val v >> 
-       putMVar svar (min 1 (readers+1), val)
-
--- | Returns 'True' if the 'SampleVar' is currently empty.
---
--- Note that this function is only useful if you know that no other
--- threads can be modifying the state of the 'SampleVar', because
--- otherwise the state of the 'SampleVar' may have changed by the time
--- you see the result of 'isEmptySampleVar'.
---
-isEmptySampleVar :: SampleVar a -> IO Bool
-isEmptySampleVar svar = do
-   (readers, _) <- readMVar svar
-   return (readers == 0)
+   val <- takeMVar svar
+   putMVar svar (max val v)
