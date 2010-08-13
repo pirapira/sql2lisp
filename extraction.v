a12b75fce7c3fdcbbaeaa00b171d3196e8d70155
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2010-08-13 12:45:34 +0900

    IO in Coq is extracted into IO in Haskell.

--- extraction.v
+++ extraction.v
@@ -6,6 +6,7 @@ Extract Constant kE => "\x->x".
 Extract Constant agent => "Agent".
 Extract Constant knowledge "'data" => "'data".
 Extract Constant U => "()".
+Extract Constant IO "a" => "IO a".
 
 Recursive Extraction motto_comm.
 
